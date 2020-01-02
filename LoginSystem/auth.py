42ee572682b09ed3bacd5ce499260a03491f7b28
Gabriel Lins <46428208+GaletTears@users.noreply.github.com>
2020-01-02 08:59:10 -0300

    Add a sign up interface

--- LoginSystem/auth.py
+++ LoginSystem/auth.py
@@ -1,8 +1,10 @@
+from flask_login import login_user
 from flask import Blueprint, render_template, redirect, request
-from LoginSystem.models import User, add_admin
+from LoginSystem.models import User
 
 auth = Blueprint('auth', __name__)
 
+# Render functions
 @auth.route('/login')
 def render_login ():
     return render_template(
@@ -11,22 +13,26 @@ def render_login ():
         script='/static/js/index.js'
     )
 
+@auth.route('/register')
+def render_register ():
+    return render_template(
+        'register.html',
+        title='Sign up',
+        script='/static/js/index.js'
+    )
 
+
+# Processing functions
 @auth.route('/login', methods=['POST', 'GET'])
 def login ():
     if ( request.method == 'POST' ):
         _user = request.form['user']
         _pass = request.form['pass']
-        # _keep = request.form['keep']
+        _keep = 'keep' in request.form
 
         user = User.query.filter_by(username=_user).first()
-        print(user.id)
-        print(user.username)
-        print(user.email)
-        print(user.password)
+        if ( user and _pass == user.password ):
+            return print('SUCCESS!!') # TODO: Add endpoint
+        else:
+            return print('ERROR!!') # TODO: Add endpoint
 
-    return render_template(
-        'login.html',
-        title='Sign in',
-        script='/static/js/index.js'
-    )
--- LoginSystem/models.py
+++ LoginSystem/models.py
@@ -3,11 +3,15 @@ from LoginSystem import db
 
 class User ( UserMixin, db.Model ):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
 
-    def __init__ ( self, username, password, email ):
+    def __init__ ( self, username, password, email, first_name, last_name ):
         self.username = username
         self.password = password
         self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
