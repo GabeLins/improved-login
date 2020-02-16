c017c0f7e6a1fc7987b42b9ea68818bfdafd554b
Gabriel Lins <46428208+GabeLins@users.noreply.github.com>
2020-02-16 14:27:57 -0300

    Create the database directory on startup

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from os import environ
 from LoginSystem import app, db
+import os
 
 if __name__ == '__main__':
+    if ( not os.path.exists('./LoginSystem/data') ):
+        os.mkdir('./LoginSystem/data')
+    
     db.create_all(app=app)
-    app.run('localhost', 4444, debug=True)
+    app.run('0.0.0.0', 4444, debug=True)
